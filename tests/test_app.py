cd80c1c9b068cb71768ad8bf81482f05ffb065b0
Tariq Tamrabet <tariq.tamrabet@polytech-lille.net>
2025-01-17 10:23:11 +0100

    pyproject.toml file changed (add ipython)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
 # tests/test_app.py
 from src.main import lin_reg  # Exemple simple, testez une fonction ou comportement
 
+
+def test_dummy():
+    assert True
+    
+    
 def test_linear_regression_coefs():
     # Vérifier que le modèle linéaire a été entraîné et possède des coefficients
     assert hasattr(lin_reg, 'coef_')
+
+
